d263285a2c9d46a96994c9a7cf07685bb6d30714
crispycret <bnadeau.dev@gmail.com>
2023-07-18 23:08:01 -0400

    Changes made to the ComingSoon Page in the frontends/client-react application

---
+++ src/components/Hubspot.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+const hubspotForm = () => {
+    // useEffect(() => {
+    //     if (window.hbspt) {
+    //       window.hbspt.forms.create({
+    //         portalId: "YOUR-PORTAL-ID",
+    //         formId: "YOUR-FORM-ID",
+    //         target: "YOUR-FORM-CONTAINER-ID"
+    //       });
+    //     }
+    //   },[window.hbspt])
+    return (
+        <></>
+    // <>
+    // <Helmet>
+    //   <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"> </script>
+    // </Helmet>
+    // </>
+    )
+}
