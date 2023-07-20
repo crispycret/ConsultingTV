fd7c413a43605c5eb2595b359f85fe988c2fb49f
crispycret <bnadeau.dev@gmail.com>
2023-07-20 19:28:29 -0400

    Added backend flask api and finished Coming Soon Page with Form integration

--- frontends/client-react/src/components/layouts/Footer.tsx
+++ frontends/client-react/src/components/layouts/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { BsLinkedin, BsGithub, BsFacebook, BsInstagram } from "react-icons/bs";
 
 export const Footer = () => {
   return (
@@ -8,7 +8,8 @@ export const Footer = () => {
       <Row>
         <Col md={12} className="mx-auto my-auto text-center">
           <p>
-            <a href="https://www.github.com/crispycret" className="text-light mx-1" ><BsGithub /></a>
+            {/* <a href="https://www.github.com/crispycret" className="text-light mx-1" ><BsGithub /></a> */}
+            <a href="https://www.facebook.com" className="text-light mx-1" ><BsFacebook /></a>
             <a href="https://www.linkedin.com/in/brandon-nadeau-705123254/" className="text-light mx-1 me-3" ><BsLinkedin /></a>
             &copy; 2023 ConsultingTV. All rights reserved. | Built with React, Twitter Bootstrap using TypeScript. <a href="https://github.com/crispycret/ConsultingTV">View Source</a>
           </p>
