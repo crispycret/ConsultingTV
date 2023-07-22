e9340081f33e3c4096d03432fde73edcded0c003
crispycret <bnadeau.dev@gmail.com>
2023-07-21 23:18:23 -0400

    Added basic error handling to the routes to stop the application from crashing upon a bad request

--- core/apis/hubspot/routes.py
+++ core/apis/hubspot/routes.py
@@ -18,31 +18,43 @@ def test():  return {"status": 200, "response": 'test'}
 
 @hubspot.route('/forms', methods=['GET'])
 def get_forms():
-    response = hubspot_api.scopes.forms.get_forms()
-    return {"status": response.status_code, "response": response.text}
-
+    try: 
+        response = hubspot_api.scopes.forms.get_forms()
+        return {"status": response.status_code, "response": response.text}
+    except Exception as e:
+        return {"status": 500, "response": str(e)}
 
 @hubspot.route('/forms/<form_id>', methods=['GET'])
 def get_form(form_id):
-    response = hubspot_api.scopes.forms.get_form(form_id)
-    return {"status": response.status_code, "response": response.text}
+    try:
+        response = hubspot_api.scopes.forms.get_form(form_id)
+        return {"status": response.status_code, "response": response.text}
+    except Exception as e:
+        return {"status": 500, "response": str(e)}
 
 
 @hubspot.route('/forms/submit/<portal_id>/<form_id>', methods=['POST'])
 def submit_form(portal_id, form_id):
     """ Given a portal_id, form_id, and form data, submit the form to HubSpot."""
-    data = request.get_json()
-    
-    # Use payload template to build payload
-    # Begining structure of payload selection that will expand to include more payload types that can be selected by the client
-    payload_type = data['payload_type'] or 'basic_n_fields_payload' # default to basic_n_fields_payload
-    if (payload_type == 'basic_n_fields_payload'):
-        payload = payloads.basic_n_fields_payload(data['fields']) 
-    else:
-        payload = payloads.basic_n_fields_payload(data['fields']) # Again default to basic_n_fields_payload for now
+    try: 
+        data = request.get_json()
+        
+        # Use payload template to build payload
+        # Begining structure of payload selection that will expand to include more payload types that can be selected by the client
+        payload_type = data['payload_type'] or 'basic_n_fields_payload' # default to basic_n_fields_payload
+        if (payload_type == 'basic_n_fields_payload'):
+            payload = payloads.basic_n_fields_payload(data['fields']) 
+        else:
+            payload = payloads.basic_n_fields_payload(data['fields']) # Again default to basic_n_fields_payload for now
 
-    payload = json.dumps(payload) # converts python dictionary into json string
-    
-    # Submit the form using the HubspotAPI object
-    response = hubspot_api.scopes.forms.submit_form(portal_id, form_id, payload)    
-    return {"status": response.status_code, "response": response.text}
+        payload = json.dumps(payload) # converts python dictionary into json string
+        
+        # Submit the form using the HubspotAPI object
+        response = hubspot_api.scopes.forms.submit_form(portal_id, form_id, payload)
+        return {"status": response.status_code, "response": response.text}
+    except Exception as e:
+        return {"status": 500, "response": str(e)}
+
+
+
+
